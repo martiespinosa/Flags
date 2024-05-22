f4d98e57ec69abfb0332e11b45693783bf398e8d
Martí Espinosa <martiespinosa99@gmail.com>
2024-05-22 21:49:36 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Martí Espinosa Farran on 22/5/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
